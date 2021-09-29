ae2e2e9cb11f47901f7f3848170780ce11db4dc7
Antonio <molina.antonio@gmail.com>
2021-09-29 15:24:38 +0200

    Update

---
+++ selection.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This script select the minimun cost for the electricity in 24h
+Usage:
+
+"""
+
+# Importo las funciones utilizadas
+# import ...
+
+
+# Importo la configuración del fichero
+from config.auth import *
+
+
+# Realizo la elección del minimo valor de la electricidad en el número de horas seleccionadas
+def main():
+    select_hours = 2
+    min_start = 0
+    price = (0.24, 0.25)
+
+
+if __name__ == "__main__":
+	main()

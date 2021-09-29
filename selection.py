b07a3c6bed406ed283f71e367641155f8e9c20d1
Antonio <molina.antonio@gmail.com>
2021-09-29 18:34:33 +0200

    Update

--- selection.py
+++ selection.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-This script select the minimun cost for the electricity in 24h
+This script select the minimun cost for the electricity for selected hours
 Usage:
 
 """
@@ -17,9 +17,25 @@ from config.auth import *
 
 # Realizo la elección del minimo valor de la electricidad en el número de horas seleccionadas
 def main():
-    select_hours = 2
-    min_start = 0
-    price = (0.24, 0.25)
+    selectHours = 2
+    minStart = 0
+    electricityPriceList = (0.240, 0.205, 0.011, 0.103, 0.034, 0.045, 0.203, 0.554, 0.231, 0.210, 0.615, 0.271, 0.362,
+    0.40, 0.232, 0.121, 0.131, 0.324, 0.745, 0.253, 0.554, 0.121, 0.871, 0.695)
+    minPriceList = (999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999,
+    999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999)
+
+
+    # Genero la lista de precios en función del número de horas seleccionadas
+    for i in electricityPriceList:
+        end = i + selectHours
+        for j in end:
+        minPriceList(i) = electricityPriceList (j)
+
+
+    # Busco el mínimo de esa lista y el índice
+    minPrice = min(minPriceList)
+    startEquip = list.index(minPrice)
+    stopEquip = startEquip + selectHours
 
 
 if __name__ == "__main__":

d794916e0ba13c7d4a67ff6c2c8653c111168624
Antonio <molina.antonio@gmail.com>
2021-09-30 16:02:24 +0200

    Update

--- selection.py
+++ selection.py
@@ -17,12 +17,15 @@ from config.auth import *
 
 # Realizo la elección del minimo valor de la electricidad en el número de horas seleccionadas
 def main():
+
+    """
+    Las cojo al final del fichero auth
     selectHours = 2
-    priceSum = 0
     electricityPriceList = [0.240, 0.205, 0.011, 0.103, 0.034, 0.045, 0.203, 0.554, 0.231, 0.210, 0.615, 0.271, 0.362,
     0.40, 0.232, 0.121, 0.131, 0.324, 0.745, 0.253, 0.554, 0.121, 0.871, 0.695]
     minPriceList = [999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0,
     999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0]
+    """
 
     for i in range(0,len(electricityPriceList) - selectHours + 1):
         start = i
